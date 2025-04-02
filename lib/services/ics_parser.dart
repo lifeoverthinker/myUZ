6ebe0d251d246c7e6c6285dba19308cb6123126f
lifeoverthinker <96396847+lifeoverthinker@users.noreply.github.com>
2025-04-02 18:21:22 +0200

    renowacja 3.2

--- lib/services/ics_parser.dart
+++ lib/services/ics_parser.dart
@@ -1,7 +1,11 @@
 import 'package:my_uz/models/zajecia_model.dart';
+import 'package:logger/logger.dart';
 
 class IcsParser {
+  static final _logger = Logger();
+
   static List<Zajecia> parsujZajecia(String icsContent, {int? nauczycielId}) {
+    _logger.i('Rozpoczęcie parsowania pliku ICS...');
     final List<Zajecia> zajecia = [];
     final lines = icsContent.split('\n');
 
@@ -58,18 +62,24 @@ class IcsParser {
       }
     }
 
+    _logger.i('Zakończenie parsowania. Znaleziono ${zajecia.length} zajęć');
     return zajecia;
   }
 
   static DateTime _parseIcsDate(String icsDate) {
-    // Format: YYYYMMDDTHHMMSSZ
-    final year = int.parse(icsDate.substring(0, 4));
-    final month = int.parse(icsDate.substring(4, 6));
-    final day = int.parse(icsDate.substring(6, 8));
-    final hour = int.parse(icsDate.substring(9, 11));
-    final minute = int.parse(icsDate.substring(11, 13));
-    final second = int.parse(icsDate.substring(13, 15));
+    try {
+      // Format: YYYYMMDDTHHMMSSZ
+      final year = int.parse(icsDate.substring(0, 4));
+      final month = int.parse(icsDate.substring(4, 6));
+      final day = int.parse(icsDate.substring(6, 8));
+      final hour = int.parse(icsDate.substring(9, 11));
+      final minute = int.parse(icsDate.substring(11, 13));
+      final second = int.parse(icsDate.substring(13, 15));
 
-    return DateTime.utc(year, month, day, hour, minute, second);
+      return DateTime.utc(year, month, day, hour, minute, second);
+    } catch (e) {
+      Logger().e('Błąd parsowania daty: $icsDate', error: e);
+      return DateTime.now(); // Awaryjnie zwracamy aktualny czas
+    }
   }
 }
